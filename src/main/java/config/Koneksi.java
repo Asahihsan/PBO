7560a083b3bd6a75a8dcc5eb17ae1fc2f78fd977
Anisaf745 <ansftyani@gmail.com>
2026-01-03 14:53:20 +0700

    update dari nisa

--- src/main/java/config/Koneksi.java
+++ src/main/java/config/Koneksi.java
@@ -14,7 +14,7 @@ public class Koneksi {
             return DriverManager.getConnection(
                 "jdbc:postgresql://localhost:5432/cafe_kopi",
                 "postgres",
-                "121334"
+                "12345678"
             );
         } catch (Exception e) {
             e.printStackTrace();
